dbeb71ce4c6bb8096af85c26076e5d2e3c4e41fa
AfzalHossainYen <161500344+AfzalHossainYen@users.noreply.github.com>
2024-10-06 16:18:09 +0600

    Add project files.

---
+++ test_dotnet1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace test_dotnet1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
